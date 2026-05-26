1d3952b070a519057aa3129531f38638ff19f25a
Adrian Wong <adrianwong135@gmail.com>
2026-05-26 12:42:21 +0100

    updated nvim

--- nvim.lua
+++ nvim.lua
@@ -62,3 +62,30 @@ vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro cul"
 
 vim.keymap.set("n", "<leader>q", ":w<CR>:Explore<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "<C-j>", function() return vim.fn.pumvisible() == 1 and "<C-n>" or "<C-j>" end, { expr = true })
+vim.keymap.set("i", "<C-k>", function() return vim.fn.pumvisible() == 1 and "<C-p>" or "<C-k>" end, { expr = true })
+
+vim.lsp.config("clangd", {
+    cmd = { "clangd", "--background-index", "--clang-tidy", "--header-insertion=iwyu" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+    root_markers = { "compile_commands.json", "compile_flags.txt", ".clangd", ".git" },
+})
+vim.lsp.enable("clangd")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local buf = args.buf
+        vim.bo[buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        local map = function(lhs, rhs) vim.keymap.set("n", lhs, rhs, { buffer = buf }) end
+        map("gd", vim.lsp.buf.definition)
+        map("gD", vim.lsp.buf.declaration)
+        map("gr", vim.lsp.buf.references)
+        map("gi", vim.lsp.buf.implementation)
+        map("K",  vim.lsp.buf.hover)
+        map("<leader>rn", vim.lsp.buf.rename)
+        map("<leader>ca", vim.lsp.buf.code_action)
+        map("<leader>f",  function() vim.lsp.buf.format({ async = true }) end)
+        map("[d", function() vim.diagnostic.jump({ count = -1 }) end)
+        map("]d", function() vim.diagnostic.jump({ count =  1 }) end)
+    end,
+})
